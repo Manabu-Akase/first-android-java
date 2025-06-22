02409358b34e6633b99a044c273d3dca69e08cae
Manabu Akase <doubt.m.39.knight@gmail.com>
2025-06-22 22:29:46 +0900

    Create MainActivity.java

---
+++ from Youtube /PaintApp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.paintapp;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyCanvas myCanvas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        //描画のId取得
+        myCanvas = (MyCanvas) findViewById(R.id.myCanvas);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+
+        //クリアメソッド呼び出し
+        public void onClear(View view){
+            myCanvas.clearCanvas();
+        }
+    }
